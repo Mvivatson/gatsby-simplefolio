b5d2bdd7538206f51951300ba2b21b79e9077960
Shakeel Khan <khanboy411@hotmail.com>
2021-04-26 23:55:07 -0700

    Edited the webpage with my information. Need to scale the iphone image down. And fill out the rest of the information.

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,38 +2,38 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Shakeel Khan | Software Engineer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'Hi! I am',
+  name: 'Shakeel Khan',
+  subtitle: 'I am a software engineer.',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
+  img: 'Shak.jpg',
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'https://skhan16.github.io/Resume/', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
+    img: 'iphone-11-Control-Page.png',
+    title: 'iMovR desk controller app',
     info: '',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/sKhan16/iMovRSwift', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
@@ -59,13 +59,13 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'khansk97@uw.edu',
 };
 
 // FOOTER DATA
 export const footerData = {
   networks: [
-    {
+    /* {
       id: nanoid(),
       name: 'twitter',
       url: '',
@@ -74,21 +74,21 @@ export const footerData = {
       id: nanoid(),
       name: 'codepen',
       url: '',
-    },
+    }, */
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/shakeel-khan-29b79a157/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/sKhan16',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
