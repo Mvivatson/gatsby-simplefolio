a148b8f21fbc7e28440b1acb8965177207edbc33
sKhan16 <khanboy411@hotmail.com>
2021-04-29 12:10:06 -0700

    change default text

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,7 +11,7 @@ export const headData = {
 export const heroData = {
   title: "Welcome! I'm",
   name: 'Shakeel Khan',
-  subtitle: ' software engineer.',
+  subtitle: 'software engineer.',
   cta: '',
 };
 
